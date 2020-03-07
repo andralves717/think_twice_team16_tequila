357f50160afec33f1dba8d44c54480cc5b01f8f4
Gabriel Saudade <gabrielfsaudade@ua.pt>
2020-03-07 23:30:28 +0000

    c21

---
+++ challenge13/main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(new File(args[0]));
+
+        Double x ,y;
+
+        x = Double.parseDouble(read.next());
+        y = Double.parseDouble(read.next());
+
+
+    }
+    
+}
