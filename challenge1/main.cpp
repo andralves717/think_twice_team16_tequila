66b153a2aa13373473243d03f4710ba54caa2c5b
André Alves <andr.alves@ua.pt>
2020-03-08 00:58:32 +0000

    correção cha1 e cha4

--- challenge1/main.cpp
+++ challenge1/main.cpp
@@ -6,21 +6,20 @@
 #include <string>
 #include <fstream>
 #include <math.h>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
     string input = argv[1];
     ifstream file_input (input);
-    string lines[3];
-    int i = 0;
+    vector<string> lines;
+    string line;
     if(file_input.is_open()){
-        while(getline(file_input,lines[i++]));
+        while(getline(file_input,line)){
+            lines.push_back(line);
+        }
         file_input.close();
-    }
-    else {
-        cout << "Esse ficheiro não existe!\n";
-        return 0;
     }
 
     ofstream file_output ("result.txt");
@@ -29,7 +28,6 @@ int main(int argc, char *argv[]) {
     int n = std::stoi (lines[0],&sz);
     int p = std::stoi (lines[1],&sz);
     if (n < 1 || p < 1 || n > 200 || p > 10101){
-        cout << "Argumentos inválidos (1<=n<=200 e 1<=p<=10101)\n";
         return 0;
     }
     int k = 0;
@@ -39,7 +37,6 @@ int main(int argc, char *argv[]) {
         }
     }
     if(k == 0 || k >= 110) {
-        cout << "Não foi encontrado nenhum número.\n";
         return 0;
     }
 
--- challenge4/main.py
+++ challenge4/main.py
@@ -4,15 +4,14 @@ import sys
 def main(argv):
     prime = []
 
-    line = []
     with open(argv[1]) as f:
         line = f.read().split(" ")
 
     num = int(line[0])
     c = int(line[1])
 
-    assert num >= 1 and num <= 1000
-    
+    assert 1 <= num <= 1000
+
     for j in range(1, num):
         for i in range(2, j//2 + 1): 
             if (j % i) == 0: 
@@ -20,13 +19,15 @@ def main(argv):
         else: 
             prime += [j]
 
-    if len(prime) % 2 == 0:
-        x = 2 * c
-        prime = prime[x:-x]
+    len_prime = len(prime)
+    half_len = int(len_prime / 2) - c
 
+    if len_prime % 2 == 0:
+        half_len = int(len_prime/2) - c
+        prime = prime[half_len:-half_len]
     else:
-        x = 2 * c - 1   
-        prime = prime[x:-x]
+        half_len = half_len + 1
+        prime = prime[half_len:-half_len]
 
     f = open("team16_tequila/challenge4/result.txt", "w")
     f.write("{} {}: {}\n".format(line[0], line[1], ' '.join(str(elem) for elem in prime)))
