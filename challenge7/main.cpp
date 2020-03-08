49fee2df1f5c4c1611baf2037b21d2cd889b16c3
André Alves <andr.alves@ua.pt>
2020-03-08 06:17:15 +0000

    challenge7

--- challenge7/main.cpp
+++ challenge7/main.cpp
@@ -11,54 +11,13 @@
 
 using namespace std;
 
-int find_kth(int *v, int n, int k) {
-    if (n == 1 && k == 0) return v[0];
+double findMedian(int a[], int n){
+    sort(a, a+n);
 
-    int m = (n + 4)/5;
-    int *medians = new int[m];
-    for (int i=0; i<m; i++) {
-        if (5*i + 4 < n) {
-            int *w = v + 5*i;
-            for (int j0=0; j0<3; j0++) {
-                int jmin = j0;
-                for (int j=j0+1; j<5; j++) {
-                    if (w[j] < w[jmin]) jmin = j;
-                }
-                swap(w[j0], w[jmin]);
-            }
-            medians[i] = w[2];
-        } else {
-            medians[i] = v[5*i];
-        }
-    }
-
-    int pivot = find_kth(medians, m, m/2);
-    delete [] medians;
-
-    for (int i=0; i<n; i++) {
-        if (v[i] == pivot) {
-            swap(v[i], v[n-1]);
-            break;
-        }
-    }
-
-    int store = 0;
-    for (int i=0; i<n-1; i++) {
-        if (v[i] < pivot) {
-            swap(v[i], v[store++]);
-        }
-    }
-    swap(v[store], v[n-1]);
-
-    if (store == k) {
-        return pivot;
-    } else if (store > k) {
-        return find_kth(v, store, k);
-    } else {
-        return find_kth(v+store+1, n-store-1, k-store-1);
-    }
+    if(n%2 != 0)
+        return (double) a[n/2];
+    return (double)(a[(n-1)/2] + a[n/2])/2.0;
 }
-
 
 int main(int argc, char *argv[]) {
     string input = argv[1];
@@ -80,7 +39,7 @@ int main(int argc, char *argv[]) {
 
     int* num_array = &nums[0];
 
-    int result = find_kth(num_array, nums.size(), nums.size()/2);
+    double result = findMedian(num_array, nums.size());
 
     if (file_output.is_open()){
         file_output << result << "\n";
